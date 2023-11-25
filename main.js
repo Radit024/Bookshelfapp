8493e4bfa0304a9b39a38cf26d58ade5f2417fc9
AthallahDzaki <athallahdzaki@gmail.com>
2023-11-25 23:38:57 +0700

    Fix

--- main.js
+++ main.js
@@ -1,10 +1,103 @@
-function addBook(title, author, pages, isComplete) {
+let bookshelf = [];
+
+document.body.onload = () => {
+  loadBookshelf();
+  renderBookshelf();
+}
+
+function renderBookshelf(findJudul = "") {
+  // Retrieve the inProgressBooks and completedBooks arrays from the getBookshelf function
+  const { inProgressBooks, completedBooks } = getBookshelf();
+
+  // Select the inProgressBooks and completedBooks lists
+  const inProgressBooksList = document.getElementById("incompleteBookshelfList");
+  const completedBooksList = document.getElementById("completeBookshelfList");
+
+  // Clear the lists
+  inProgressBooksList.innerHTML = "";
+  completedBooksList.innerHTML = "";
+
+  // For each book in the inProgressBooks array, create an li element and append it to the inProgressBooks list
+  inProgressBooks.forEach((book) => {
+    if(findJudul == "")
+    {
+      const bookItem = document.createElement("article");
+      bookItem.classList.add("book_item");
+      bookItem.innerHTML = `
+      <h3>${book.title}</h3>
+      <p>Penulis : ${book.author}</p>
+      <p>Tahun : ${book.year}</p>
+      <div class="action">
+        <button class="green" onclick="moveBook(${book.id}, true)">Selesai Dibaca</button>
+        <button class="red" onclick="deleteBook(${book.id})">Hapus Buku</button>
+      </div>
+      `;
+      inProgressBooksList.appendChild(bookItem);
+    }
+    else
+    {
+      if(book.title.toLowerCase().includes(findJudul.toLowerCase()))
+      {
+        const bookItem = document.createElement("article");
+        bookItem.classList.add("book_item");
+        bookItem.innerHTML = `
+        <h3>${book.title}</h3>
+        <p>Penulis : ${book.author}</p>
+        <p>Tahun : ${book.year}</p>
+        <div class="action">
+          <button class="green" onclick="moveBook(${book.id}, true)">Selesai Dibaca</button>
+          <button class="red" onclick="deleteBook(${book.id})">Hapus Buku</button>
+        </div>
+        `;
+        inProgressBooksList.appendChild(bookItem);
+      }
+    }
+  });
+
+  // For each book in the completedBooks array, create an li element and append it to the completedBooks list
+  completedBooks.forEach((book) => {
+    if(findJudul == "") {
+      const bookItem = document.createElement("article");
+      bookItem.classList.add("book_item");
+      bookItem.innerHTML = `
+      <h3>${book.title}</h3>
+      <p>Penulis : ${book.author}</p>
+      <p>Tahun : ${book.year}</p>
+      <div class="action">
+        <button class="green" onclick="moveBook(${book.id}, false)">Belum Selesai Dibaca</button>
+        <button class="red" onclick="deleteBook(${book.id})">Hapus Buku</button>
+      </div>
+      `;
+      completedBooksList.appendChild(bookItem);
+    }
+    else
+    {
+      if(book.title.toLowerCase().includes(findJudul.toLowerCase()))
+      {
+        const bookItem = document.createElement("article");
+        bookItem.classList.add("book_item");
+        bookItem.innerHTML = `
+        <h3>${book.title}</h3>
+        <p>Penulis : ${book.author}</p>
+        <p>Tahun : ${book.year}</p>
+        <div class="action">
+          <button class="green" onclick="moveBook(${book.id}, false)">Belum Selesai Dibaca</button>
+          <button class="red" onclick="deleteBook(${book.id})">Hapus Buku</button>
+        </div>
+        `;
+        completedBooksList.appendChild(bookItem);
+      }
+    }
+  });
+}
+
+function addBook(title, author, year, isComplete) {
   // Create a new book object
   const newBook = {
     id: Date.now(),
     title,
     author,
-    pages,
+    year,
     isComplete,
   };
 
@@ -73,4 +166,31 @@ function loadBookshelf() {
 
   // Assign the current bookshelf array to the bookshelf variable
   bookshelf = currentBookshelf;
+}
+
+function SubmitInputBook(evt) {
+  // Prevent the default form submit behavior
+  evt.preventDefault();
+
+  // Get the values from the title, author, and pages fields
+  const title = document.getElementById("inputBookTitle").value;
+  const author = document.getElementById("inputBookAuthor").value;
+  const year = document.getElementById("inputBookYear").value;
+  const isComplete = document.getElementById("inputBookIsComplete").checked;
+
+  // Add the new book to the bookshelf
+  addBook(title, author, year, isComplete);
+
+  // Reset the form
+  evt.target.reset();
+}
+
+function findJudul(evt) {
+  evt.preventDefault();
+  
+  const findJudul = document.getElementById("searchBookTitle").value;
+  renderBookshelf(findJudul);
+
+  // Reset the form
+  evt.target.reset();
 }
