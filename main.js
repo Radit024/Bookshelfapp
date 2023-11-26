6f30c3bcd14dc8b802e531bde92772d99173da64
Daffa Radityo Adjiefirmansyah <95222352+Radit024@users.noreply.github.com>
2023-11-26 18:55:06 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,24 +3,19 @@ let bookshelf = [];
 document.body.onload = () => {
   loadBookshelf();
   renderBookshelf();
-}
+};
 
 function renderBookshelf(findJudul = "") {
-  // Retrieve the inProgressBooks and completedBooks arrays from the getBookshelf function
   const { inProgressBooks, completedBooks } = getBookshelf();
 
-  // Select the inProgressBooks and completedBooks lists
-  const inProgressBooksList = document.getElementById("incompleteBookshelfList");
+  const inProgressBooksList = document.getElementById(
+    "incompleteBookshelfList"
+  );
   const completedBooksList = document.getElementById("completeBookshelfList");
-
-  // Clear the lists
   inProgressBooksList.innerHTML = "";
   completedBooksList.innerHTML = "";
-
-  // For each book in the inProgressBooks array, create an li element and append it to the inProgressBooks list
   inProgressBooks.forEach((book) => {
-    if(findJudul == "")
-    {
+    if (findJudul == "") {
       const bookItem = document.createElement("article");
       bookItem.classList.add("book_item");
       bookItem.innerHTML = `
@@ -29,15 +24,13 @@ function renderBookshelf(findJudul = "") {
       <p>Tahun : ${book.year}</p>
       <div class="action">
         <button class="green" onclick="moveBook(${book.id}, true)">Selesai Dibaca</button>
-        <button class="red" onclick="deleteBook(${book.id})">Hapus Buku</button>
+        <button class="red" onclick="createModalForDelete(${book.id})">Hapus Buku</button>
+        <button class="blue" onclick="createModalForEdit(${book.id})">Edit Buku</button>
       </div>
       `;
       inProgressBooksList.appendChild(bookItem);
-    }
-    else
-    {
-      if(book.title.toLowerCase().includes(findJudul.toLowerCase()))
-      {
+    } else {
+      if (book.title.toLowerCase().includes(findJudul.toLowerCase())) {
         const bookItem = document.createElement("article");
         bookItem.classList.add("book_item");
         bookItem.innerHTML = `
@@ -46,17 +39,16 @@ function renderBookshelf(findJudul = "") {
         <p>Tahun : ${book.year}</p>
         <div class="action">
           <button class="green" onclick="moveBook(${book.id}, true)">Selesai Dibaca</button>
-          <button class="red" onclick="deleteBook(${book.id})">Hapus Buku</button>
+          <button class="red" onclick="createModalForDelete(${book.id})">Hapus Buku</button>
+          <button class="blue" onclick="createModalForEdit(${book.id})">Edit Buku</button>
         </div>
         `;
         inProgressBooksList.appendChild(bookItem);
       }
     }
   });
-
-  // For each book in the completedBooks array, create an li element and append it to the completedBooks list
   completedBooks.forEach((book) => {
-    if(findJudul == "") {
+    if (findJudul == "") {
       const bookItem = document.createElement("article");
       bookItem.classList.add("book_item");
       bookItem.innerHTML = `
@@ -65,15 +57,13 @@ function renderBookshelf(findJudul = "") {
       <p>Tahun : ${book.year}</p>
       <div class="action">
         <button class="green" onclick="moveBook(${book.id}, false)">Belum Selesai Dibaca</button>
-        <button class="red" onclick="deleteBook(${book.id})">Hapus Buku</button>
+        <button class="red" onclick="createModalForDelete(${book.id})">Hapus Buku</button>
+        <button class="blue" onclick="createModalForEdit(${book.id})">Edit Buku</button>
       </div>
       `;
       completedBooksList.appendChild(bookItem);
-    }
-    else
-    {
-      if(book.title.toLowerCase().includes(findJudul.toLowerCase()))
-      {
+    } else {
+      if (book.title.toLowerCase().includes(findJudul.toLowerCase())) {
         const bookItem = document.createElement("article");
         bookItem.classList.add("book_item");
         bookItem.innerHTML = `
@@ -82,7 +72,8 @@ function renderBookshelf(findJudul = "") {
         <p>Tahun : ${book.year}</p>
         <div class="action">
           <button class="green" onclick="moveBook(${book.id}, false)">Belum Selesai Dibaca</button>
-          <button class="red" onclick="deleteBook(${book.id})">Hapus Buku</button>
+          <button class="red" onclick="createModalForDelete(${book.id})">Hapus Buku</button>
+          <button class="blue" onclick="createModalForEdit(${book.id})">Edit Buku</button>
         </div>
         `;
         completedBooksList.appendChild(bookItem);
@@ -91,8 +82,19 @@ function renderBookshelf(findJudul = "") {
   });
 }
 
+function editBook(id, title, author, year) {
+  const bookIndex = bookshelf.findIndex((book) => book.id === id);
+  if (bookIndex !== -1) {
+    bookshelf[bookIndex].title = title;
+    bookshelf[bookIndex].author = author;
+    bookshelf[bookIndex].year = year;
+
+    localStorage.setItem("bookshelf", JSON.stringify(bookshelf));
+  }
+  renderBookshelf();
+}
+
 function addBook(title, author, year, isComplete) {
-  // Create a new book object
   const newBook = {
     id: Date.now(),
     title,
@@ -101,28 +103,21 @@ function addBook(title, author, year, isComplete) {
     isComplete,
   };
 
-  // Add the new book to the bookshelf array
   bookshelf.push(newBook);
 
-  // Save the updated bookshelf array to localStorage
   localStorage.setItem("bookshelf", JSON.stringify(bookshelf));
 
-  // Render the updated bookshelf
   renderBookshelf();
 }
 
 function getBookshelf() {
-  // Retrieve the bookshelf array from localStorage
   const storedBookshelf = JSON.parse(localStorage.getItem("bookshelf"));
 
-  // If the bookshelf array exists in localStorage, use it. Otherwise, use an empty array.
   const currentBookshelf = storedBookshelf !== null ? storedBookshelf : [];
 
-  // Filter the current bookshelf array into two arrays: inProgressBooks and completedBooks
   const inProgressBooks = currentBookshelf.filter((book) => !book.isComplete);
   const completedBooks = currentBookshelf.filter((book) => book.isComplete);
 
-  // Return an object containing the two arrays
   return {
     inProgressBooks,
     completedBooks,
@@ -130,67 +125,140 @@ function getBookshelf() {
 }
 
 function moveBook(bookId, isComplete) {
-  // Find the book with the specified bookId in the bookshelf array
   const bookIndex = bookshelf.findIndex((book) => book.id === bookId);
 
-  // If the book is found, update its isComplete property and save the updated bookshelf array to localStorage
   if (bookIndex !== -1) {
     bookshelf[bookIndex].isComplete = isComplete;
     localStorage.setItem("bookshelf", JSON.stringify(bookshelf));
   }
 
-  // Render the updated bookshelf
   renderBookshelf();
 }
 
 function deleteBook(bookId) {
-  // Find the book with the specified bookId in the bookshelf array
   const bookIndex = bookshelf.findIndex((book) => book.id === bookId);
 
-  // If the book is found, remove it from the bookshelf array and save the updated bookshelf array to localStorage
   if (bookIndex !== -1) {
     bookshelf.splice(bookIndex, 1);
     localStorage.setItem("bookshelf", JSON.stringify(bookshelf));
   }
 
-  // Render the updated bookshelf
   renderBookshelf();
 }
 
 function loadBookshelf() {
-  // Retrieve the bookshelf array from localStorage
   const storedBookshelf = JSON.parse(localStorage.getItem("bookshelf"));
 
-  // If the bookshelf array exists in localStorage, use it. Otherwise, use an empty array.
   const currentBookshelf = storedBookshelf !== null ? storedBookshelf : [];
 
-  // Assign the current bookshelf array to the bookshelf variable
   bookshelf = currentBookshelf;
 }
 
 function SubmitInputBook(evt) {
-  // Prevent the default form submit behavior
   evt.preventDefault();
 
-  // Get the values from the title, author, and pages fields
   const title = document.getElementById("inputBookTitle").value;
   const author = document.getElementById("inputBookAuthor").value;
-  const year = document.getElementById("inputBookYear").value;
+  const year = parseInt(document.getElementById("inputBookYear").value);
   const isComplete = document.getElementById("inputBookIsComplete").checked;
 
-  // Add the new book to the bookshelf
   addBook(title, author, year, isComplete);
 
-  // Reset the form
   evt.target.reset();
 }
 
 function findJudul(evt) {
   evt.preventDefault();
-  
+
   const findJudul = document.getElementById("searchBookTitle").value;
   renderBookshelf(findJudul);
 
-  // Reset the form
   evt.target.reset();
+}
+
+function createModalForDelete(id) {
+  document.getElementById("modal").innerHTML = `
+  <div id="id01" class="modal" style="display:block">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <form class="modal-content">
+        <div class="container">
+          <h1>Hapus Buku</h1>
+          <p>Apakah anda setuju untuk menghapus buku ini?</p>
+    
+          <div class="clearfix">
+            <button type="button" class="cancelbtn" onclick="document.getElementById('id01').style.display='none'">Cancel</button>
+            <button type="button" class="deletebtn" onclick="deleteBook(${id}); document.getElementById('id01').style.display='none'">Delete</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  `;
+}
+
+function SubmitEditBook(evt) {
+  evt.preventDefault();
+
+  const title = document.getElementById("editBookJudul").value;
+  const author = document.getElementById("editBookAuthor").value;
+  const year = parseInt(document.getElementById("editBookYear").value);
+  const bookID = parseInt(document.getElementById("BookID").value);
+  console.log({
+    title,
+    author,
+    year,
+    bookID,
+  });
+  editBook(bookID, title, author, year);
+
+  evt.target.reset();
+}
+
+function createModalForEdit(id) {
+  const bookIndex = bookshelf.findIndex((book) => book.id === id);
+
+  document.getElementById("modal").innerHTML = `
+  <div id="id01" class="modal" style="display:block">
+    <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+    <form class="modal-content" onsubmit="SubmitEditBook(event)">
+      <div class="container">
+        <h1>Edit Buku</h1>
+
+        <label for="judulBuku"><b>Judul Buku</b></label>
+        <input id="editBookJudul" type="text" placeholder="Masukkan Nama Buku" name="judulBuku" value="${bookshelf[bookIndex].title}" required>
+
+        <label for="penulis"><b>Penulis Buku</b></label>
+        <input id="editBookAuthor" type="text" placeholder="Masukkan Penulis Buku" name="penulis" value="${bookshelf[bookIndex].author}" required>
+
+        <label for="tahun"><b>Tahun Terbit</b></label>
+        <input id="editBookYear" on type="number" placeholder="Masukkan Tahun Terbit" oninput="onlynumber(this)" value="${bookshelf[bookIndex].year}" name="tahun" required>
+
+        <input hidden value="${id}" id="BookID">
+
+        <button type="submit" onclick="document.getElementById('id01').style.display='none'">Edit</button>
+      </div>
+    </form>
+  </div>
+  `;
+}
+
+var modal = document.getElementById("id01");
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+function onlynumber(input) {
+  let ip = input.id;
+  let res = ip.value;
+
+  if (res != "") {
+    if (isNaN(res)) {
+      ip.value = "";
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
